9095f5eb6bdd4c3fd5106db4e4a38cc07791dadd
Jaakko Yli-Luukko <jaakko.yli@gmail.com>
2021-08-14 11:53:41 +0300

    .

--- pi-steer/test_bno085.py
+++ pi-steer/test_bno085.py
@@ -1,74 +1,29 @@
 import time
-from math import atan2, asin, pi, degrees
-from board import SCL, SDA
-from busio import I2C
-from adafruit_bno08x import (
-    BNO_REPORT_ACCELEROMETER, BNO_REPORT_GYROSCOPE, BNO_REPORT_MAGNETOMETER, BNO_REPORT_LINEAR_ACCELERATION,
-    BNO_REPORT_ROTATION_VECTOR, BNO_REPORT_GAME_ROTATION_VECTOR, BNO_REPORT_GEOMAGNETIC_ROTATION_VECTOR, 
-    BNO_REPORT_STEP_COUNTER, BNO_REPORT_RAW_ACCELEROMETER, BNO_REPORT_RAW_GYROSCOPE, BNO_REPORT_RAW_MAGNETOMETER,
-    BNO_REPORT_SHAKE_DETECTOR, BNO_REPORT_STABILITY_CLASSIFIER, BNO_REPORT_ACTIVITY_CLASSIFIER, 
-    BNO_REPORT_GYRO_INTEGRATED_ROTATION_VECTOR
-)
-from adafruit_bno08x.i2c import BNO08X_I2C
+import pi_steer.bno085
+import numpy
 
-# FEATURES = [BNO_REPORT_ACCELEROMETER, BNO_REPORT_GYROSCOPE, BNO_REPORT_MAGNETOMETER, BNO_REPORT_LINEAR_ACCELERATION,
-#     BNO_REPORT_ROTATION_VECTOR, BNO_REPORT_GAME_ROTATION_VECTOR, BNO_REPORT_GEOMAGNETIC_ROTATION_VECTOR, 
-#     BNO_REPORT_STEP_COUNTER, BNO_REPORT_RAW_ACCELEROMETER, BNO_REPORT_RAW_GYROSCOPE, BNO_REPORT_RAW_MAGNETOMETER,
-#     BNO_REPORT_SHAKE_DETECTOR, BNO_REPORT_STABILITY_CLASSIFIER, BNO_REPORT_ACTIVITY_CLASSIFIER, 
-#     BNO_REPORT_GYRO_INTEGRATED_ROTATION_VECTOR]
+bno = pi_steer.bno085.BNO085()
 
-FEATURES = [
-    BNO_REPORT_ROTATION_VECTOR
-    ]
-
-
-def enable_features(bno):
-    for feature in FEATURES:
-        try:
-            bno.enable_feature(feature)
-        except Exception as err:
-            print('BNO085 feature failed:', err)
-
-def init():
-    print('Init BNO085')
-    try:
-        i2c = I2C(SCL, SDA, frequency=400000)
-        bno = BNO08X_I2C(i2c, address=0x4b)
-    except Exception as err:
-        print('BNO085 I2C failed', err)
-        return (None, None)
-
-    enable_features(bno)
-    return (i2c, bno)
-
-(i2c, bno) = init()
+headings = []
+rolls = []
+pitches = []
 
 while True:
-    try:
-        (qx, qy, qz, qw) = bno.quaternion
-    except:
-        time.sleep(1)
-        (ic2, bno) = init()
-        continue
-    sinr_cosp = 2 * (qw * qx + qy * qz)
-    cosr_cosp = 1 - 2 * (qx * qx + qy * qy)
-    roll = degrees(atan2(sinr_cosp, cosr_cosp))
+    tic = time.time()
+    (heading, roll, pitch) = bno.read()
+    headings.append(heading)
+    rolls.append(roll)
+    pitches.append(pitch)
 
-    sinp = 2 * (qw * qy - qz * qx)
-    try:
-        pitch = asin(sinp)
-    except ValueError:
-        print('\n',qx, qy, qz, qw,'\n')
-        pitch = pi/2
-    pitch = degrees(pitch)
+    min_heading = numpy.amin(headings)
+    max_heading = numpy.amax(headings)
+    min_roll = numpy.amin(rolls)
+    max_roll = numpy.amax(rolls)
+    min_pitch = numpy.amin(pitches)
+    max_pitch = numpy.amax(pitches)
 
-    siny_cosp = 2 * (qw * qz + qx * qy)
-    cosy_cosp = 1 - 2 * (qy * qy + qz * qz)
-    heading = degrees(atan2(siny_cosp, cosy_cosp))
-    if heading < 0:
-        heading += 360
+    print('\rMin H {: = 7.2f} P {: = 7.2f} R {: = 7.2f} Max H {: = 7.2f} P {: = 7.2f} R {: = 7.2f}   '.format(min_heading, min_pitch, min_roll, max_heading, max_pitch, max_roll), end='')
 
-    print('\r H {: = 7.2f} P {: = 7.2f} R {: = 7.2f}'.format(heading, pitch, roll), end='')
 
-    time.sleep(0.1)
+    time.sleep(0.01)
 
