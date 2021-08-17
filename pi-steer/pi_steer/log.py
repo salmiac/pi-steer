7e14062161f5536e9b8552ae2ba70d5bf56bf0a5
Jaakko Yli-Luukko <jaakko.yli@gmail.com>
2021-08-17 21:24:16 +0300

    debug logging

--- pi-steer/pi_steer/log.py
+++ pi-steer/pi_steer/log.py
@@ -1,15 +1,17 @@
 import csv
+import datetime
 
 class Log():
     def __init__(self, name) -> None:
         self.data = []
         self.count = 0
-        self.name = name
+        date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S ")
+        self.name = date + name
 
     def log_csv(self, data):
         self.count += 1
         self.data.append(data)
-        if self.count % 10:
+        if self.count % 100:
             try:
                 with open(self.name+'.csv', 'a') as file:
                     writer = csv.writer(file)
