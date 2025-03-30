eb5c52184181e09a73bd3e0b519d9fdffd9c31f1
Jaakko Yli-Luukko <jaakko.yli@gmail.com>
2025-03-30 18:37:15 +0300

    timing in ADC

--- pi-steer-rust/src/hw/ads1115.rs
+++ pi-steer-rust/src/hw/ads1115.rs
@@ -1,18 +1,20 @@
 use rppal::i2c::I2c;
 use std::error::Error;
+use std::thread;
+use std::time::Duration;
 
 const ADS111X_ADDRESS0: u16 = 0x48;
 const CONVERSION_REGISTER: u8 = 0x00;
 const CONFIG_REGISTER: u8 = 0x01;
-const CONFIG_HIGH: u8 = 0b1100_0001;
-const CONFIG_LOW: u8 = 0b1000_0011;
+const CONFIG_HIGH: u8 = 0b1000_0001;
+const CONFIG_LOW: u8 = 0b1110_0011;
 /* Configuration
 bit description
 15      1b start single conversion
 14:12   1xxb channel 0 - 3
 11:9    000b FSR = ±6.144V
 8       1b Single-shot mode or power-down state 
-7:5     100b : 128SPS 
+7:5     111b : 860SPS 
 4       0b : Traditional comparator
 3       0b : Active low 
 2       0b : Nonlatching comparator.
@@ -31,7 +33,8 @@ impl ADS1115 {
     }
 
     pub fn read(&mut self, channel: u8) -> Result<f32, Box<dyn Error>> {
-        self.i2c.write(&[CONFIG_REGISTER, ((channel & 0b000_0011) << 4) | CONFIG_HIGH, CONFIG_LOW])?;
+        self.i2c.write(&[CONFIG_REGISTER, ((channel & 0b000_0111) << 4) | CONFIG_HIGH, CONFIG_LOW])?;
+        thread::sleep(Duration::from_micros(1000));
         let mut data = [0u8; 2];
         self.i2c.write_read(&[CONVERSION_REGISTER], &mut data)?;
         Ok(i16::from_be_bytes(data) as f32 / 32767.0 * 6.144) // Return voltage
@@ -45,7 +48,12 @@ mod tests {
 
     #[test]
     fn ads() {
-        let ads = ADS1115::new();
-        println!("ADS read: {:?}", ads.expect("REASON").read(1));
+        let mut ads = ADS1115::new();
+        println!("ADS read: {:?}", ads.as_mut().expect("REASON").read(0));
+        println!("ADS read: {:?}", ads.as_mut().expect("REASON").read(1));
+        println!("ADS read: {:?}", ads.as_mut().expect("REASON").read(0));
+        println!("ADS read: {:?}", ads.as_mut().expect("REASON").read(1));
+        println!("ADS read: {:?}", ads.as_mut().expect("REASON").read(0));
+        println!("ADS read: {:?}", ads.as_mut().expect("REASON").read(1));
     }
 }
